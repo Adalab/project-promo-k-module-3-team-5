91a8d73ee88dd6c9aa61221062f51c2145751100
maribelgr <mgrageraruiz@gmail.com>
2020-11-03 14:01:04 +0100

    Add header react

--- my-react-project/src/components/App.js
+++ my-react-project/src/components/App.js
@@ -1,7 +1,26 @@
-import "../stylesheets/App.css";
+import React from "react";
+import "../stylesheets/App.scss";
+import logo from "../images/awesome-1.svg";
 
-function App() {
-  return <div className="App"></div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <header className="header">
+        <a href="./index.html">
+          <img
+            src={logo}
+            alt="Logo Awesome Profile Cards"
+            title="Awesome Profile Cards"
+            className="header__logo"
+          />
+        </a>
+      </header>
+    );
+  }
 }
 
 export default App;
