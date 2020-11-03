9f10ebcdf8c05f04c60a8a954f36dea889936481
maribelgr <mgrageraruiz@gmail.com>
2020-11-03 15:33:05 +0100

    Add footer react

--- my-react-project/src/components/App.js
+++ my-react-project/src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../stylesheets/App.scss";
 import logo from "../images/awesome-1.svg";
+import logoFooter from "../images/emyem.png";
 
 class App extends React.Component {
   constructor(props) {
@@ -9,16 +10,27 @@ class App extends React.Component {
 
   render() {
     return (
-      <header className="header">
-        <a href="./index.html">
+      <>
+        <header className="header">
+          <a href="./index.html">
+            <img
+              src={logo}
+              alt="Logo Awesome Profile Cards"
+              title="Awesome Profile Cards"
+              className="header__logo"
+            />
+          </a>
+        </header>
+        <footer role="contentinfo" className="footer">
+          <small className="footer__small">Awesome profile-cards @2020</small>
           <img
-            src={logo}
-            alt="Logo Awesome Profile Cards"
-            title="Awesome Profile Cards"
-            className="header__logo"
+            src={logoFooter}
+            alt="Logo de Adalab"
+            title="Adalab, escuela digital especializada para mujeres"
+            className="footer__logo"
           />
-        </a>
-      </header>
+        </footer>
+      </>
     );
   }
 }
