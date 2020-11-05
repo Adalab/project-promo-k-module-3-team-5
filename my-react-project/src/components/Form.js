b0cc7654bbc8cee9d237c2f058fff2a0111bd1c6
maribelgr <mgrageraruiz@gmail.com>
2020-11-05 13:25:49 +0100

    Add design component

--- my-react-project/src/components/Form.js
+++ my-react-project/src/components/Form.js
@@ -1,86 +1,14 @@
 import React from "react";
-import designIcon from "../images/design.svg";
 import keyboardIcon from "../images/keyboard.svg";
 import shareIcon from "../images/share.svg";
+import Design from "./Design";
 import "../stylesheets/pages/_main.scss";
 
 class Form extends React.Component {
   render() {
     return (
       <nav className="gen-main__customize">
-        <section className="gen-main__customize-design">
-          <div
-            className="customize-design__container js-collapsable"
-            tabindex="2"
-          >
-            <img
-              className="customize-design__container-logo"
-              src={designIcon}
-              alt="Share logo"
-            />
-            <h2 className="customize-design__container-title">Diseña</h2>
-            <button className="customize-design__container-btn js-arrow"></button>
-          </div>
-
-          <form
-            className="customize-design__colors js__hidden js-section"
-            action=""
-            method="GET"
-          >
-            <h2 className="customize-design__colors-title">Colores</h2>
-            <label
-              className="customize-design__colors1 js-customize-design__colors"
-              for="colors1"
-              id="customize-design__colors1"
-            >
-              <input
-                className="customize-design__colors1-selection js-customize-design__colors1-selection"
-                type="radio"
-                id="colors1"
-                value="greenpalette"
-                name="colors"
-              />
-
-              <div className="customize-design__colors1-boxes1"></div>
-              <div className="customize-design__colors1-boxes2"></div>
-              <div className="customize-design__colors1-boxes3"></div>
-            </label>
-            <label
-              className="customize-design__colors2 js-customize-design__colors"
-              for="colors2"
-              id="customize-design__colors2"
-            >
-              <input
-                className="customize-design__colors2-selection js-customize-design__colors1-selection"
-                type="radio"
-                id="colors2"
-                value="orangepalette"
-                name="colors"
-              />
-              <div className="customize-design__colors2-boxes1"></div>
-              <div className="customize-design__colors2-boxes2"></div>
-              <div className="customize-design__colors2-boxes3"></div>
-            </label>
-            <label
-              className="customize-design__colors3 js-customize-design__colors"
-              for="colors3"
-              id="customize-design__colors3"
-            >
-              <input
-                className="customize-design__colors3-selection js-customize-design__colors1-selection"
-                type="radio"
-                id="colors3"
-                value="bluepalette"
-                name="colors"
-              />
-              <div className="customize-design__colors3-boxes1"></div>
-              <div className="customize-design__colors3-boxes2"></div>
-              <div className="customize-design__colors3-boxes3"></div>
-            </label>
-          </form>
-        </section>
-
-        {/* section rellena */}
+        <Design />
         <section className="gen-main__customize-menu">
           <div className="customize-menu__container js-collapsable">
             <img
