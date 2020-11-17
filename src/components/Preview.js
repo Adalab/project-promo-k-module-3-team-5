cd62ce3ce4edc7d53e6aa514076c9705341505ec
maribelgr <mgrageraruiz@gmail.com>
2020-11-17 13:01:50 +0100

    Add profile image

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,9 +1,9 @@
 import React from "react";
-import cardImg1 from "../images/pin-long-brown-hair.png";
-import cardImg2 from "../images/pin-glasses.png";
-import cardImg3 from "../images/pin-dark-hair.png";
-import cardImg4 from "../images/pin-lights.png";
-import cardImg5 from "../images/pin-long-hair.png";
+// import cardImg1 from "../images/pin-long-brown-hair.png";
+// import cardImg2 from "../images/pin-glasses.png";
+// import cardImg3 from "../images/pin-dark-hair.png";
+// import cardImg4 from "../images/pin-lights.png";
+// import cardImg5 from "../images/pin-long-hair.png";
 import Profile from "./Profile";
 import "../stylesheets/pages/_main.scss";
 
@@ -21,40 +21,7 @@ class Preview extends React.Component {
             <h2 className="card__occupation js-occupation">
               {this.props.data.position || "Front-end developer"}
             </h2>
-            <div className="card__photo js-image js__profile-image">
-              <div className="js-div">
-                <img
-                  className="card__img-1"
-                  src={cardImg1}
-                  alt="Card Photo"
-                  title="Imagen por defecto"
-                />
-                <img
-                  className="card__img-2"
-                  src={cardImg2}
-                  alt="Card Photo"
-                  title="Imagen por defecto"
-                />
-                <img
-                  className="card__img-3"
-                  src={cardImg3}
-                  alt="Card Photo"
-                  title="Imagen por defecto"
-                />
-                <img
-                  className="card__img-4"
-                  src={cardImg4}
-                  alt="Card Photo"
-                  title="Imagen por defecto"
-                />
-                <img
-                  className="card__img-5"
-                  src={cardImg5}
-                  alt="Card Photo"
-                  title="Imagen por defecto"
-                />
-              </div>
-            </div>
+            <Profile />
             <nav className="card__contact">
               <div className="card__contact-container">
                 <a
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,15 +1,47 @@
 import React, { Component } from "react";
+import cardImg1 from "../images/pin-long-brown-hair.png";
+import cardImg2 from "../images/pin-glasses.png";
+import cardImg3 from "../images/pin-dark-hair.png";
+import cardImg4 from "../images/pin-lights.png";
+import cardImg5 from "../images/pin-long-hair.png";
 import PropTypes from "prop-types";
 
 class Profile extends Component {
   render() {
-    const { avatar } = this.props;
     return (
-      <div className="profile">
-        <div
-          className="profile__avatar"
-          style={{ backgroundImage: `url(${avatar})` }}
-        ></div>
+      <div className="card__photo js-image js__profile-image">
+        <div className="js-div">
+          <img
+            className="card__img-1"
+            src={cardImg1}
+            alt="Card Photo"
+            title="Imagen por defecto"
+          />
+          <img
+            className="card__img-2"
+            src={cardImg2}
+            alt="Card Photo"
+            title="Imagen por defecto"
+          />
+          <img
+            className="card__img-3"
+            src={cardImg3}
+            alt="Card Photo"
+            title="Imagen por defecto"
+          />
+          <img
+            className="card__img-4"
+            src={cardImg4}
+            alt="Card Photo"
+            title="Imagen por defecto"
+          />
+          <img
+            className="card__img-5"
+            src={cardImg5}
+            alt="Card Photo"
+            title="Imagen por defecto"
+          />
+        </div>
       </div>
     );
   }
